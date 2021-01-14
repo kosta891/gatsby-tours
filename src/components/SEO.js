70f9a3c3cbc39c0c4ee4515cffad2d83d008d765
kosta891 <kosta891@gmail.com>
2021-01-14 20:36:26 +0100

    facebook

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -26,10 +26,21 @@ const SEO = ({title, description}) => {
             <meta name='description' content={description || siteDesc} />
             <meta name='image' content={image || null} />
 
+            {/* facebook */}
+            <meta property="og:url" content={siteUrl} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={siteTitle} />
+            <meta property="og:description" content={siteDesc} />
+            <meta property="og:image" content={`${siteUrl}${image}`} />
+            <meta property="og:image:width" content="400"/>
+            <meta property="og:image:height" content="300"/>
+            
+
+
             <meta name ="twitter:card" content="summary_large_image" />
             <meta name ="twitter:creator" content={twitterUsername} />
             <meta name ="twitter:title" content={siteTitle} />
-            <meta name ="twitter:description" content={description} />
+            <meta name ="twitter:description" content={siteDesc  } />
             <meta name ="twitter:image" content={`${siteUrl}${image}`} />
 
         </Helmet>
