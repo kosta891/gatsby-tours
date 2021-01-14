24eb46db6ffd60a2ac3a362882b341fb35d169c9
kosta891 <kosta891@gmail.com>
2021-01-14 20:25:26 +0100

    twiiter

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -25,6 +25,13 @@ const SEO = ({title, description}) => {
         <Helmet htmlAttributes={{lang:'en'}} title={`${title} | ${siteTitle}`}>
             <meta name='description' content={description || siteDesc} />
             <meta name='image' content={image || null} />
+
+            <meta name ="twitter:card" content="summary_large_image" />
+            <meta name ="twitter:creator" content={twitterUsername} />
+            <meta name ="twitter:title" content={siteTitle} />
+            <meta name ="twitter:description" content={description} />
+            <meta name ="twitter:image" content={`${siteUrl}${image}`} />
+
         </Helmet>
     )
 }
